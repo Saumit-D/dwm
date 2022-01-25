94b932219c3f7f6fd7baee6567b9adb97e8b274c
Saumit Dinesan <79687674+Saumit-D@users.noreply.github.com>
2022-01-25 23:34:15 +0530

    Update config.h

--- config.h
+++ config.h
@@ -35,8 +35,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	
 	{ "Gimp",         NULL,      NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox",      NULL,      NULL,           0 << 8,    0,          0,          -1,        -1 },
+	{ "Microsoft Teams",NULL,    NULL,	     4,		0,	    0,		 0,  	   -1 },
+	{ "Microsoft Teams Notification",NULL,    NULL,	     0,		1,	    0,		 0,  	   -1 },
+	{ "Spotify",	  NULL,	     NULL,	     9,		0,	    0,		 0,  	   -1 },
 	{ "St",           NULL,      NULL,           0,         0,          1,           0,        -1 },
 	{ "st-256color",  NULL,      NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,           NULL,      "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
